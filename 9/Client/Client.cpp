5c6a4123c70b15bebde726cc024723255dd7ee15
zavarushka7 <zavarushka7>
2024-04-28 12:13:02 +0500

    second

---
+++ 9/Client/Client.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+#include <Winsock2.h>
+#include <Ws2tcpip.h>
+#pragma comment(lib, "Ws2_32.lib")
+using namespace std;
+class TCPClient {
+private:
+    SOCKET sock;
+    struct sockaddr_in serv_addr;
+    string filename;
+
+public:
+    TCPClient(const char* address, int port) {
+        WSADATA wsaData;
+        cout << "Enter the file name: ";
+        cin >> filename;
+        if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+            std::cerr << "WSAStartup failed." << std::endl;
+            exit(EXIT_FAILURE);
+        }
+
+        if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+            std::cerr << "Socket creation failed." << std::endl;
+            WSACleanup();
+            exit(EXIT_FAILURE);
+        }
+
+        serv_addr.sin_family = AF_INET;
+        serv_addr.sin_port = htons(port);
+
+        if (inet_pton(AF_INET, address, &serv_addr.sin_addr) <= 0) {
+            std::cerr << "Invalid address: Address not supported." << std::endl;
+            closesocket(sock);
+            WSACleanup();
+            exit(EXIT_FAILURE);
+        }
+
+        if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+            std::cerr << "Connection Failed." << std::endl;
+            closesocket(sock);
+            WSACleanup();
+            exit(EXIT_FAILURE);
+        }
+
+        if (send(sock, filename.c_str(), filename.size(), 0) == SOCKET_ERROR) {
+            cerr << "Send failed" << endl;
+        }
+        else {
+            cout << "Filename sent" << endl;
+            char buffer[4096];
+            int bytesReceived = recv(sock, buffer, sizeof(buffer), 0);
+            if (bytesReceived > 0) {
+                cout << "Received file content: " << endl;
+                cout.write(buffer, bytesReceived);
+                cout << endl;
+            }
+        }
+
+        closesocket(sock);
+        WSACleanup();
+        cout << "Press enter to exit...";
+        cin.get();
+    }
+};
+int main() {
+    TCPClient client("127.0.0.1", 8080);
+    cin.get();
+    return 0;
+}
---
+++ 9/Server/Server.cpp
@@ -0,0 +1,103 @@
+﻿#include <iostream>
+#include <Winsock2.h>
+#include <Ws2tcpip.h>
+#pragma comment(lib, "Ws2_32.lib")
+#include <thread>
+#include <fstream>
+using namespace std;
+class TCPServer {
+private:
+    SOCKET server_fd;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    const int port = 8080;
+    void handle_client(SOCKET client_socket) {
+        char filename[1024] = { 0 };
+        int bytes_received = recv(client_socket, filename, sizeof(filename), 0);
+        if (bytes_received == SOCKET_ERROR) {
+            cerr << "recv failed with error" << WSAGetLastError() << endl;
+            closesocket(client_socket);
+        }
+        else {
+            cout << "Filename received " << filename << endl;
+            ifstream file(filename, ios::binary | ios::ate);
+            if (!file.is_open()) {
+                cerr << "Failed to open file :" << filename << endl;
+            }
+            else {
+                streamsize size = file.tellg();
+                file.seekg(0, ios::beg);
+                char* buffer = new char[size];
+                if (file.read(buffer, size)) {
+                    send(client_socket, buffer, size, 0);
+                }
+                delete[] buffer;
+                file.close();
+            }
+        }
+        closesocket(client_socket);
+    }
+public:
+    TCPServer() {
+        WSADATA wsaData;
+        int opt = 1;
+
+        if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+            std::cerr << "WSAStartup failed with error: " << WSAGetLastError() << std::endl;
+            exit(EXIT_FAILURE);
+        }
+
+        if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+            std::cerr << "Socket creation failed with error: " << WSAGetLastError() << std::endl;
+            WSACleanup();
+            exit(EXIT_FAILURE);
+        }
+
+        address.sin_family = AF_INET;
+        address.sin_addr.s_addr = INADDR_ANY;
+        address.sin_port = htons(port);
+
+        if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, (char*)&opt, sizeof(opt)) == SOCKET_ERROR) {
+            std::cerr << "setsockopt failed with error: " << WSAGetLastError() << std::endl;
+            closesocket(server_fd);
+            WSACleanup();
+            exit(EXIT_FAILURE);
+        }
+
+        if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) == SOCKET_ERROR) {
+            std::cerr << "Bind failed with error: " << WSAGetLastError() << std::endl;
+            closesocket(server_fd);
+            WSACleanup();
+            exit(EXIT_FAILURE);
+        }
+
+        if (listen(server_fd, 3) == SOCKET_ERROR) {
+            std::cerr << "Listen failed with error: " << WSAGetLastError() << std::endl;
+            closesocket(server_fd);
+            WSACleanup();
+            exit(EXIT_FAILURE);
+        }
+    }
+    void run() {
+        std::cout << "Waiting for connections..." << std::endl;
+
+        while (true) {
+            SOCKET client_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+            if (client_socket == INVALID_SOCKET) {
+                std::cerr << "Accept failed with error: " << WSAGetLastError() << std::endl;
+                continue;
+            }
+            thread clientThread(&TCPServer::handle_client, this, client_socket);
+            clientThread.detach();
+        }
+    }
+    ~TCPServer() {
+        closesocket(server_fd);
+        WSACleanup();
+    }
+};
+int main() {
+    TCPServer server;
+    server.run();
+    return 0;
+}
